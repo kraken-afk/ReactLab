8fcb5523df2149519f6d7eebdd2a17dbbb4c6fef
Romeo Noveanre <noveanrer@gmail.com>
2024-04-24 11:26:39 +0700

    Initialize with React + SWC + Typescript + TailwindCSS + Vite

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import "normalize.css";
+import "@/base.css";
+
+export function App() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <h1 className="text-xl">Hello World</h1>
+    </div>
+  )
+}
